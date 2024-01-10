78b25e383e4084cd9cb96d2c531cb7ad8bf98b08
Ayrton <ayrtton.vinicius@gmail.com>
2024-01-10 18:02:42 -0300

    Set the basic HTML structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,25 @@
+import Illustration from "../assets/images/illustration-article.svg";
+import Avatar from "../assets/images/image-avatar.webp";
+
 function Card() {
     return (
         <main>
-            Learning
-            Published 21 Dec 2023
-            HTML & CSS foundations
-            These languages are the backbone of every website, defining structure, content, and presentation.
-            Greg Hooper
+            <div id="image">
+                <img src={Illustration} alt="" srcSet="" />
+            </div>
+            <div id="details">
+                Learning
+                Published 21 Dec 2023
+            </div>
+            <div id="content">
+                <h1>HTML & CSS foundations</h1>
+                <p>These languages are the backbone of every website, defining 
+                    structure, content, and presentation.</p>
+            </div>
+            <div id="author">
+                <img src={Avatar} alt="" srcSet="" />
+                Greg Hooper
+            </div>
         </main>
     )
 }
