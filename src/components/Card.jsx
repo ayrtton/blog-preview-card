7a9a8e070399cc36ce504152393d7a3765218493
Ayrton <ayrtton.vinicius@gmail.com>
2024-01-11 18:11:37 -0300

    Create a mixin for flexboxes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,21 +4,23 @@ import Avatar from "../assets/images/image-avatar.webp";
 function Card() {
     return (
         <main>
-            <div id="image">
-                <img src={Illustration} alt="" srcSet="" />
-            </div>
-            <div id="details">
-                Learning
-                Published 21 Dec 2023
-            </div>
-            <div id="content">
-                <h1>HTML & CSS foundations</h1>
-                <p>These languages are the backbone of every website, defining 
-                    structure, content, and presentation.</p>
-            </div>
-            <div id="author">
-                <img src={Avatar} alt="" srcSet="" />
-                Greg Hooper
+            <div id="card">
+                <div id="image">
+                    <img src={Illustration} alt="" srcSet="" />
+                </div>
+                <div id="details">
+                    Learning
+                    Published 21 Dec 2023
+                </div>
+                <div id="content">
+                    <h1>HTML & CSS foundations</h1>
+                    <p>These languages are the backbone of every website, defining 
+                        structure, content, and presentation.</p>
+                </div>
+                <div id="author">
+                    <img src={Avatar} alt="" srcSet="" />
+                    Greg Hooper
+                </div>
             </div>
         </main>
     )
